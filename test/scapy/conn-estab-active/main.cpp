ff5ae00a9c5c2cb133bd074860a8ec52b92af7aa
klewy <cigankov807@gmail.com>
2026-03-16 11:50:26 +0300

    fix

--- test/scapy/conn-estab-active/main.cpp
+++ test/scapy/conn-estab-active/main.cpp
@@ -1,19 +1,17 @@
 // ...
 
 int main() {
-    TcpListener listener{};
-    listener.bind(8090);
-    listener.listen(999);
-    std::println("user: bound and listening");
-    auto sock = listener.accept();
-    std::println("user: accepted");
+    sleep(3); // Wait for py test thing to start
+    TcpSocket sock{};
+    sock.connect("10.0.0.1", 8090);
+
     while (true) {
         std::array<char, 512> buf{};
         auto rd = sock.read(buf.data(), buf.size());
+        std::println("user: rd {}", rd);
         if (rd == 0) {
-            std::println("user: DATA FINISHED, CLOSING...");
+            std::println("user: FIN");
             break;
         }
     }
 }
-
